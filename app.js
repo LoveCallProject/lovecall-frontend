0fc9a4d06f64efb89066648cb2e13fe77cee17ea
disoul <starky_ds@sina.cn>
2015-11-24 19:54:37 +0800

    add timelineparser

--- app.js
+++ app.js
@@ -117,6 +117,180 @@
     }
   };
 
+  var beatAdd = function(one, other) {
+    var newMeasure = one[0] + other[0];
+    var newBeat = one[1] + other[1];
+    newMeasure += Math.floor(newBeat / 16);
+    return [newMeasure, newBeat % 16];
+  };
+
+  var beatCompare = function(one, other) {
+    return (one[0] * 16 + one[1]) - (other[0] * 16 + other[1]);
+  };
+
+  var makeEngineEvent = function(a, tempo) {
+    return {
+      "ts": tempo.beatToTime(a[0][0], a[0][1]),
+      "type": a[1],
+      "params": a[2]
+    };
+  };
+
+  var parseFuFuAction = function(startBeat, params) {
+    return [
+      [startBeat, "Fu!", null],
+      [beatAdd(startBeat, [0, 2]), "Fu!", null]
+    ];
+  };
+
+  var parsePeriodicAction = function(startBeat, endBeat, type, offset, increment) {
+    var result = [];
+    var currentBeat = beatAdd(startBeat, offset);
+
+    while (beatCompare(currentBeat, endBeat) < 0) {
+      result.push([currentBeat, type, null]);
+      currentBeat = beatAdd(currentBeat, increment);
+    }
+
+    return result;
+  };
+
+  var parseSJAction = function(startBeat, endBeat, params) {
+    return parsePeriodicAction(startBeat, endBeat, "上举", [0, 0], [0, params[0]]);
+  };
+
+  var parseLDAction = function(startBeat, endBeat, params) {
+    return parsePeriodicAction(startBeat, endBeat, "里打", [0, 4], [0, 8]);
+  };
+
+  var parseLTAction = function(startBeat, endBeat, params) {
+    return parsePeriodicAction(startBeat, endBeat, "里跳", [0, 4], [0, 8]);
+  };
+
+  var parseAlarmAction = function(startBeat, endBeat, params) {
+    return [
+      [startBeat, "Hi!", null],
+      [beatAdd(startBeat, [0, 8]), "Hi!", null],
+      [beatAdd(startBeat, [1, 0]), "Hi!", null],
+      [beatAdd(startBeat, [1, 4]), "Hi!", null],
+      [beatAdd(startBeat, [1, 8]), "Hi!", null],
+      [beatAdd(startBeat, [1, 12]), "Hi!", null]
+    ];
+  };
+
+  var parsePPPHAction = function(startBeat, endBeat, params) {
+    var ppphVariant = params[0];
+    var currentBeat = startBeat;
+    var result = [];
+
+    switch(ppphVariant) {
+      case 'OOOH': {
+        while (beatCompare(currentBeat, endBeat) < 0) {
+          result.push([currentBeat, "Oh~", null]);
+          result.push([beatAdd(currentBeat, [0, 12]), "Hi!", null]);
+          currentBeat = beatAdd(currentBeat, [1, 0]);
+        }
+
+        break;
+      }
+
+      default:
+        console.error('unrecognized PPPH parameter', params);
+    }
+
+    return result;
+  };
+
+  var POINT_ACTION_PARSERS = {
+    'fufu': parseFuFuAction
+  };
+
+  var LONG_ACTION_PARSERS = {
+    '上举': parseSJAction,
+    '里打': parseLDAction,
+    '警报': parseAlarmAction,
+    'PPPH': parsePPPHAction,
+    '里跳': parseLTAction
+  };
+
+  var parsePointAction = function(actionData) {
+    var actionStartBeat = actionData.slice(0, 2);
+    var actionType = actionData[2];
+    var actionParams = actionData.slice(3);
+
+    var parseFn = POINT_ACTION_PARSERS[actionType];
+    if (parseFn) {
+      return parseFn(actionStartBeat, actionParams);
+    }
+
+    console.error('unrecognized point action', actionData);
+    return [];
+  };
+
+  var parseLongAction = function(actionData) {
+    var actionStartBeat = actionData.slice(0, 2);
+    var actionEndBeat = actionData.slice(2, 4);
+    var actionType = actionData[4];
+    var actionParams = actionData.slice(5);
+
+    var parseFn = LONG_ACTION_PARSERS[actionType];
+    if (parseFn) {
+      return parseFn(actionStartBeat, actionEndBeat, actionParams);
+    }
+
+    console.error('unrecognized long action', actionData);
+    return [];
+  };
+
+  var parseTimelineAction = function(action) {
+    var actionFlag = action[0];
+    var actionData = action.slice(1);
+    switch (actionFlag) {
+      case 0:
+        return parsePointAction(actionData);
+      case 1:
+        return parseLongAction(actionData);
+    }
+
+    console.error('unrecognized action', action);
+    return [];
+  };
+
+  var parseTimeline = function(timeline, tempo) {
+    var events = timeline.map(function(v) {
+      return parseTimelineAction(v, tempo);
+    });
+
+    var result = events.reduce(function(acc, elem) {
+      var i = 0;
+      for (; i < elem.length; i++) {
+        acc.push(makeEngineEvent(elem[i], tempo));
+      }
+
+      return acc;
+    }, []);
+
+    result.sort(function(a, b) {
+      return a.ts - b.ts;
+    });
+    return result;
+  };
+
+  var parseCall = function(data) {
+    var sources = data.metadata.song.sources;
+    var offsetMs = sources[Object.keys(sources)[0]].offset +
+                 data.metadata.song.timing[0][0];
+    var beatMs = 60000 / data.metadata.song.timing[0][1];
+    var tempo = tempoFactory(beatMs, offsetMs);
+
+    var events = parseTimeline(data.timeline, tempo);
+
+    // TODO
+    return events;
+  };
+
+  console.log(parseCall(CALL_DATA));
+
   window.requestAnimationFrame(frameCallback);
 })(document, window);
 /* @license-end */
