db06a4250d4ecf760aa129aa191fa3eea05bc897
disoul <starky_ds@sina.cn>
2015-12-09 15:37:54 +0800

    ui/call: add canvas

--- src/js/ui/call.js
+++ src/js/ui/call.js
@@ -19,21 +19,93 @@ var mod = angular.module('lovecall/ui/call', [
 
 mod.controller('CallController', function($scope, $window, $log, AudioEngine, Choreography, FrameManager, ResizeDetector) {
   $log.debug('$scope=', $scope);
+
+  var events = [];
+  var preCallDrawTime = 0;
+
   $scope.$on('audio:loaded', function(e) {
+    var callCanvas = new callCanvasState();
     var callEventCallback = function(nowevent, lookaheadEvent, prevEvent) {
-      $log.debug('now:', nowevent[0].type, 'lookahead', lookaheadEvent, 'prev', prevEvent);
+     /*
+      $log.debug(
+        'now:', nowevent[0].type, nowevent[0].ts, 
+        'lookahead', lookaheadEvent, 'prev', prevEvent
+        );
+        */
+        lookaheadEvent.map(function(event) {
+          nowevent.push(event);
+        });
+        events = nowevent;
+        callCanvas.draw(events, true);
     };
+
     var queueEngine = queue.queueEngineFactory(
         Choreography.getEvents(), 
         callEventCallback, 
         $log,
         false
         );
+
     var callFrameCallback = function(ts) {
       queueEngine.update(AudioEngine.getPlaybackPosition(), true);
+      if (ts - preCallDrawTime >= 16.66) {
+        callCanvas.draw(events, false);
+        preCallDrawTime = ts;
+      }
     };
     FrameManager.addFrameCallback(callFrameCallback);
+
   });
+
+
+  /* canvas */
+  function callCanvasState() {
+    var elem = document.getElementById('call__canvas');
+    var ctx = elem.getContext('2d');
+    var circleRadius = 50;
+    var circleMargin = 10;
+    var w, h;
+    var currentTime = 0;
+    var preDrawTime = 0;
+    var tempo = Choreography.getTempo();
+    var pixPreSec = ( circleRadius * 2 + circleMargin ) / (tempo.stepToTime(0,4) - tempo.stepToTime(0, 2)) ;
+    var preStates = [];
+
+    this.draw = function(events, flag) {
+      var canvasRect = elem.getBoundingClientRect();
+      w = canvasRect.width | 0;
+      h = canvasRect.height | 0;
+      elem.width = w;
+      elem.height = h;
+      var y = h / 2;
+      currentTime = AudioEngine.getPlaybackPosition();
+      // draw 
+      ctx.clearRect(0, 0, w, h);
+      preStates = [];
+      events.map(function(event, index) {
+        var remainedTime = event.ts - currentTime;
+        var x = pixPreSec * remainedTime;
+        ctx.beginPath();
+        ctx.fillStyle = "#ff6666";
+        ctx.arc(x, y, circleRadius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = "#000";
+        ctx.fillText(event.type, x, y);
+        preStates.push({
+            ts: event.ts,
+            type: event.type,
+            position: {
+                "x": x,
+                "y": y
+              }
+            });
+      });
+    };
+    
+  };
+
+
 
 });
 /* @license-end */
