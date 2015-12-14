f4c3a383a7fea365660a27451accc2c32ee7138c
Wang Xuerui <idontknw.wang@gmail.com>
2015-12-14 23:28:24 +0800

    data: migrate Susume Tomorrow skeleton table

---
+++ src/js/data/susutomo.js
@@ -0,0 +1,32 @@
+module.exports = {
+	"lovecall": 0,
+	"metadata": {
+		"song": {
+			"title": "ススメ→トゥモロウ",
+			"artist": "μ's",
+			"album": "START:DASH!!",
+			"sources": {
+				"md5:c58b2892e24d0239a6f31b769c230dac": {
+					"offset": 5134
+				}
+			},
+			"timing": [
+				[0, 100.0, 4, 4, 0],
+				[21178, 190.0, 4, 4, 9]
+			]
+		},
+		"palette": [
+			"#ffa500",
+			"#eeeeee",
+			"#0000ff"
+		]
+	},
+	"form": [
+	],
+	"colors": [
+		[0, 0, -1, -1, -1]
+	],
+	"timeline": [
+		[1, 0, 0, 16, 0, "上举", 16]
+	]
+};
