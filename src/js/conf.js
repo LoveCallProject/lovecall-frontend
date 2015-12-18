7b415c40a9c1edd882c7d9ac3c27eababdfe77ca
Wang Xuerui <idontknw.wang@gmail.com>
2015-12-18 19:21:28 +0800

    add global config

---
+++ src/js/conf.js
@@ -0,0 +1,43 @@
+/* @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later */
+'use strict';
+
+require('angular');
+
+
+var mod = angular.module('lovecall/conf', [
+]);
+
+mod.factory('LCConfig', function() {
+  var VERSION = '20151218-dev';
+  var HASH = __webpack_hash__;
+
+
+  var getAudioBufferSize = function() {
+    // TODO: make this configurable
+    return 2048;
+  };
+
+
+  var getGlobalOffsetMs = function() {
+    // TODO
+    return 0;
+  };
+
+
+  var getKnownLocalSongs = function() {
+    // TODO
+    return [];
+  };
+
+
+  return {
+    VERSION: VERSION,
+    HASH: HASH,
+    getAudioBufferSize: getAudioBufferSize,
+    getGlobalOffsetMs: getGlobalOffsetMs,
+    getKnownLocalSongs: getKnownLocalSongs,
+  };
+});
+/* @license-end */
+
+// vim:set ai et ts=2 sw=2 sts=2 fenc=utf-8:
