1d01e518046b2aab5ab0e9350d648ab8ff7f6326
Wang Xuerui <idontknw.wang@gmail.com>
2015-12-18 20:08:31 +0800

    lovecall.config: signify importance of trailing slash

--- lovecall.config.example.js
+++ lovecall.config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // 远程音乐文件 URL 前缀
-  // URL prefix for remote music files
+  // 远程音乐文件 URL 前缀, 一定要以斜线结束
+  // URL prefix for remote music files, MUST end with a slash
   remoteMusicPrefix: '/music/',
 };
