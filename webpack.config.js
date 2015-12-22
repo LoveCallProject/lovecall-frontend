690b739ba574de60dfaeb4055aec4d63514565af
Wang Xuerui <idontknw.wang@gmail.com>
2015-12-22 21:13:56 +0800

    webpack: allow bundling opus files

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
       { test: /\.scss$/, loader: "style!css!sass" },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.tmpl\.html$/, loader: "ng-cache" },
-      { test: /\.svg$/, loader: "url-loader?limit=100000" },
+      { test: /\.(svg|opus)$/, loader: "url-loader?limit=100000" },
     ]
   }
 };
