1863bedcf54cb672f1aea2447bba6df807afb3a2
disoul <starky_ds@sina.cn>
2016-01-01 15:00:12 +0800

    don't use cordova in defalut

--- src/js/update.js
+++ src/js/update.js
@@ -11,43 +11,46 @@ var mod = angular.module('lovecall/update', [
 ]);
 
 mod.run(function($http, $mdDialog, LCConfig) {
-  $http({
-    method: 'GET',
-    url: 'http://lovecall.moe/static/version.json'
-  }).then(function successCallback(res) {
-    var versionInfo = res.data;
-    console.log(versionInfo);
-    if (versionInfo.version > LCConfig.VERSION) {
-      var confirm = $mdDialog.confirm()
-          .title('LoveCall有新版本v' + versionInfo.version + '，是否更新')
-          .textContent('更新内容：' + versionInfo.content)
-          .ariaLabel('Lucky day')
-          .ok('立即更新')
-          .openFrom({
-            top: document.body.height / 2,
-            width: 30,
-            height: 80
-          })
-          .closeTo({
-            top: document.body.height / 2
-          })
-          .cancel('下次再说')
+  // only check in cordova
+  if (window.cordova !== undefined) {
+    $http({
+      method: 'GET',
+      url: 'http://lovecall.moe/static/version.json'
+    }).then(function successCallback(res) {
+      var versionInfo = res.data;
+      console.log(versionInfo);
+      if (versionInfo.version > LCConfig.VERSION) {
+        var confirm = $mdDialog.confirm()
+            .title('LoveCall有新版本v' + versionInfo.version + '，是否更新')
+            .textContent('更新内容：' + versionInfo.content)
+            .ariaLabel('Lucky day')
+            .ok('立即更新')
+            .openFrom({
+              top: document.body.height / 2,
+              width: 30,
+              height: 80
+            })
+            .closeTo({
+              top: document.body.height / 2
+            })
+            .cancel('下次再说')
 
-      $mdDialog.show(confirm).then(function() {
-        window.cordova.plugins.FileOpener.openFile(
-            LCConfig.REMOTE_APK_PREFIX + 'lovecall' + versionInfo.version + '.apk',
-            function() {
-              // success
-            }
-        );
+        $mdDialog.show(confirm).then(function() {
+          window.cordova.plugins.FileOpener.openFile(
+              LCConfig.REMOTE_APK_PREFIX + 'lovecall' + versionInfo.version + '.apk',
+              function() {
+                // success
+              }
+          );
 
-        console.log("let's update");
-      }, function() {});
-    }
+          console.log("let's update");
+        }, function() {});
+      }
 
-  }, function errorCallback(res) {
+    }, function errorCallback(res) {
 
-  });
+    });
+  }
 });
 /* @license-end */
 
